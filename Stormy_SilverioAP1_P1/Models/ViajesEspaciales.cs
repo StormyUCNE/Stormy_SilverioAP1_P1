e71c19fc194d0e87f926861505e73df86467c214
StormyUCNE <stormy_silverio@ucne.edu.do>
2026-02-05 19:52:53 -0400

    Agrega el modelo ViajesEspaciales.

---
+++ Stormy_SilverioAP1_P1/Models/ViajesEspaciales.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Stormy_SilverioAP1_P1.Models;
+public class ViajesEspaciales
+{
+    [Key]
+    public int VueloId { get; set; }
+
+    [Required(ErrorMessage = "Campo Obligatorio")]
+    public DateTime Fecha { get; set; }
+
+    [Required(ErrorMessage = "Campo Obligatorio")]
+    public string Descripcion { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Campo Obligatorio")]
+    [Range(1, Double.MaxValue, ErrorMessage = "Campo no debe contener valores negativos.")]
+    public double Cost { get; set; }
+}
