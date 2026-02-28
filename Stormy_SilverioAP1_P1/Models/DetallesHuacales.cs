cc6b1de061bb73947d94bc6f8de4b25a5992e7de
StormyUCNE <stormy_silverio@ucne.edu.do>
2026-02-28 02:06:34 -0400

    Crea el modelo DetallesHuacales.

---
+++ Stormy_SilverioAP1_P1/Models/DetallesHuacales.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Stormy_SilverioAP1_P1.Models;
+
+public class DetallesHuacales
+{
+    [Key]
+    public int DetalleId { get; set; }
+
+    [Required(ErrorMessage = "Campo Obligatorio")]
+    public int EntradaId { get; set; }
+
+    [Required(ErrorMessage = "Campo Obligatorio")]
+    public int TipoId { get; set; }
+
+    [Required(ErrorMessage = "Campo Obligatorio")]
+    [Range(1, int.MaxValue, ErrorMessage = "Cantidad debe de ser mayor a cero")]
+    public int Cantidad { get; set; }
+
+    [Required(ErrorMessage = "Campo Obligatorio")]
+    [Range(1, Double.MaxValue, ErrorMessage = "Cantidad debe de ser mayor a cero")]
+    public decimal Precio { get; set; }
+
+    [ForeignKey("EntradaId")]
+    public EntradasHuacales? EntradasHuacales { get; set; }
+
+    [ForeignKey("TipoId")]
+    public TiposHuacales? TiposHuacales { get; set; }
+}
