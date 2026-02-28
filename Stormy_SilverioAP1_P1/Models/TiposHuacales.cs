41a8137d372383ff0a1e3992c658d26bb3df920d
StormyUCNE <stormy_silverio@ucne.edu.do>
2026-02-28 02:10:27 -0400

    Agrega el modelo TiposHuacales.

---
+++ Stormy_SilverioAP1_P1/Models/TiposHuacales.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Stormy_SilverioAP1_P1.Models;
+
+public class TiposHuacales
+{
+    [Key]
+    public int TipoId { get; set; }
+
+    [Required(ErrorMessage = "Este Campo es Requerido")]
+    public string Descripcion { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Este Campo es Requerido")]
+    [Range(1, int.MaxValue, ErrorMessage = "Cantidad debe de ser mayor a cero")]
+    public int Existencia { get; set; }
+}
