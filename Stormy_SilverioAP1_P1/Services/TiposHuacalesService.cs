bf3183bc0f41a1d8fc9791f6a3fa0656e5c893c8
StormyUCNE <stormy_silverio@ucne.edu.do>
2026-02-28 02:05:54 -0400

    Agrega el método Listar.

---
+++ Stormy_SilverioAP1_P1/Services/TiposHuacalesService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Stormy_SilverioAP1_P1.DAL;
+using Stormy_SilverioAP1_P1.Models;
+using System.Linq.Expressions;
+namespace Stormy_SilverioAP1_P1.Services;
+public class TiposHuacalesService(IDbContextFactory<Contexto> DbFactory)
+{
+    public async Task<List<TiposHuacales>> Listar(Expression<Func<TiposHuacales, bool>> criterio)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.TiposHuacales.Where(criterio).AsNoTracking().ToListAsync();
+    }
+}
